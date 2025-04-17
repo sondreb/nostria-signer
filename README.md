479f1f65ace44e30732fb0849b4db71a7eb36fa4
SondreB <sondre@outlook.com>
2025-04-17 12:24:36 +0200

    Update dependencies and remove background service
    - Updated Tauri and related plugin dependencies to their latest versions.
    - Removed unnecessary permissions and background service from AndroidManifest.xml and BackgroundService.kt.
    - Simplified MainActivity by removing background service management.
    - Refactored delete_private_key function to use delete_credential instead of delete_password.
    - Removed keep_awake command for mobile platforms as it is no longer needed.

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ Desktop: https://github.com/sondreb/nostria-signer/releases
 ## Recommended IDE Setup
 
 [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer) + [Angular Language Service](https://marketplace.visualstudio.com/items?itemName=Angular.ng-template).
+
+## Tauri notes
+
+Check crates here: https://crates.io/crates/tauri
--- src-tauri/gen/android/app/src/main/java/app/nostria/signer/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/app/nostria/signer/MainActivity.kt
@@ -1,23 +1,3 @@
 package app.nostria.signer
 
-import android.content.Intent
-import android.os.Bundle
-import com.tauri.android.TauriActivity
-
-class MainActivity : TauriActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startBackgroundService()
-    }
-
-    private fun startBackgroundService() {
-        val serviceIntent = Intent(this, BackgroundService::class.java)
-        startService(serviceIntent)
-    }
-
-    override fun onDestroy() {
-        val serviceIntent = Intent(this, BackgroundService::class.java)
-        stopService(serviceIntent)
-        super.onDestroy()
-    }
-}
+class MainActivity : TauriActivity()
